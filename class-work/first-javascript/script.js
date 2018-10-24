b4fb75f396a553bdd1f341287299c08db7821c5e
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2018-10-23 21:32:00 -0400

    In class JS exercises

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,6 @@
+var name='ramsey'
+alert(name)
+
+document.querySelector('h1').style.color='red';
+document.querySelector('#title').style.color='red';
+document.querySelector('#text-chunk').style.color='red';
