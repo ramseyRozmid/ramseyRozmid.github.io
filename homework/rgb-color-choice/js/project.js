779e2acbc0a2600a7343d2b575ffe22e60167c8c
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2018-10-24 20:46:30 -0400

    Final RGB Code

--- homework/rgb-color-choice/js/project.js
+++ homework/rgb-color-choice/js/project.js
@@ -1,14 +1,22 @@
-/*
-  Project:
-    RGB Color Choice
+const functionRGB = () => {
+  var red = document.querySelector('#red').value || 0
+  var green = document.querySelector('#green').value || 0
+  var blue = document.querySelector('#blue').value  || 0
 
-  Desired Functionality:
-    1. User enters three values into each respective input: #red, #green, #blue
-    2. User clicks "Change the Color!" button, afterwhich steps 3 and 4 occur
-    3. The content within #colorful-text needs to update to reflect the user's input
-      ex: If the user enters: 143, 56, 200 then #colorful-text should contain: rgb(143, 56, 200)
-    4. The background color of #wrapper changes to the rgb-value the user entered
-      ex: If the user enters: 143, 56, 200 then the background of #wrapper should be rgb(143, 56, 200)
-  Hints:
-    1. You will have to rely on string concatenation to build the rgb-value given the user's input: 'rgb(' + ...
-*/
+  document.querySelector('#colorful-text').innerHTML = 'rgb(' + red + ',' + green + ',' + blue + ')'
+  document.querySelector('#wrapper').style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')'
+
+  if (red >255) {
+    alert('Max value is 255 for each color input!')
+  }
+
+  if (green >255) {
+    alert('Max value is 255 for each color input!')
+  }
+
+  if (blue >255) {
+    alert('Max value is 255 for each color input!')
+  }
+}
+
+document.querySelector('#color-button').onclick = functionRGB
