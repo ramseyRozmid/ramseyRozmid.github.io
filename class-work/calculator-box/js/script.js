ba5a9b102aa0312fe1ed598d12482443d8461ffa
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2018-10-31 19:00:56 -0400

    Cleaning up/finalizing the box calculator

--- class-work/calculator-box/js/script.js
+++ class-work/calculator-box/js/script.js
@@ -1,21 +1,43 @@
- /*
-	
-	Add .click() handlers for each of the boxes:
+// 	Project Goals:
+//	A) Boxes #a10, #a20, and #a30 should each add 10, 20, and 30, respectively, to the value in the center box, #out
+//	B) Boxes #m10, #m20, and #m30 should each subtract 10, 20, and 30, respectively, from the value in the center box, #out
+//	C) #red and #blue should change the background color of #out to red and blue, respectively
+//	D) #out should change the background of #out to white, and set the value back to zero
 
-	A) Boxes #a10, #a20, and #a30 should each add 10, 20, and 30, respectively, to the value in the center box, #out
+var count = 0
+  // var boxColor
 
-	B) Boxes #m10, #m20, and #m30 should each subtract 10, 20, and 30, respectively, from the value in the center box, #out
+function calcCenterBox() {
+  count = parseInt($(this).html())+count
+  $('#out').html(count)
+}
 
-	C) #red and #blue should change the background color of #out to red and blue, respectively
-	
-	D) #out should change the background of #out to white, and set the value back to zero
+function colorCenterBoxRed() {
+  $('#out').css('backgroundColor','red')
+}
 
-	HINT: define a variable, var count = 0 outside of any functions. Use this variable to keep track of what to display inside of #out
-	Ex: when the user clicks #a10, add 10 to the "count" and then reflect this change in the HTML
+function colorCenterBoxBlue() {
+  $('#out').css('backgroundColor','blue')
+}
 
-	jQuery Methods Needed:
-	- .click()
-	- .html()
-	- .css()
+function resetCenterBox() {
+  count = 0
+  $('#out').css('backgroundColor','white')
+  $('#out').html(count)
+}
 
-*/
+$('#a10, #a20, #a30, #m10, #m20, #m30').click(calcCenterBox)
+$('#red').click(colorCenterBoxRed)
+$('#blue').click(colorCenterBoxBlue)
+$('#out').click(resetCenterBox)
+
+//
+//   function colorCenterBox (){
+//     boxColor = $(this).val().toLowerCase()
+//     console.log(boxColor)
+//
+//      css('background-color',(this id))
+//      $('#out').css(boxColor)
+//          console.log(this)
+//  }
+//  $('#red, #blue').click(colorCenterBox)
