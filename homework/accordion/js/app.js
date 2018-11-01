6f9ab6dd62d995bd5a1ef7a9d09d13d89dfa5d0c
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2018-10-31 20:40:28 -0400

    Accordian HW Code

--- homework/accordion/js/app.js
+++ homework/accordion/js/app.js
@@ -4,3 +4,9 @@
 // 2. Associate this .click() handler with a function (Make sure to think of a proper name for this function)
 // 3. Inside the body of this function, use $(this) as a starting point to find the <div class="wrapper">
 // 4. Once you find the .wrapper, use the .slideToggle() function to trigger the desired behavior
+
+function functionName() {
+  $(this).find('.wrapper').slideToggle()
+}
+
+$('.row').click(functionName)
