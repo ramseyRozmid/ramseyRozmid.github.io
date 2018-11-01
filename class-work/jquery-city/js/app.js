de92438b8dd03ea052050088f75ed8ed36dda894
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2018-10-31 20:24:25 -0400

    Using $(this) to refactor solution :)

--- class-work/jquery-city/js/app.js
+++ class-work/jquery-city/js/app.js
@@ -1,23 +1,33 @@
 // 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
 
-$('#first').click(useFirstImage)
-$('#second').click(useSecondImage)
-$('#third').click(useThirdImage)
-$('#fourth').click(useFourthImage)
+
+
+// $('#first').click(useFirstImage)
+// $('#second').click(useSecondImage)
+// $('#third').click(useThirdImage)
+// $('#fourth').click(useFourthImage)
 // 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
 
-function useFirstImage() {
-  $('#bigimage').attr('src','img/1.jpg')
+// function useFirstImage() {
+//   $('#bigimage').attr('src','img/1.jpg')
+// }
+//
+// function useSecondImage() {
+//   $('#bigimage').attr('src','img/2.jpg')
+// }
+//
+// function useThirdImage() {
+//   $('#bigimage').attr('src','img/3.jpg')
+// }
+//
+// function useFourthImage() {
+//   $('#bigimage').attr('src','img/4.jpg')
+// }
+
+function changeImage() {
+  var source = $(this).attr('src')
+
+  $('#bigimage').attr('src',source)
 }
 
-function useSecondImage() {
-  $('#bigimage').attr('src','img/2.jpg')
-}
-
-function useThirdImage() {
-  $('#bigimage').attr('src','img/3.jpg')
-}
-
-function useFourthImage() {
-  $('#bigimage').attr('src','img/4.jpg')
-}
+$('.thumb').click(changeImage)
