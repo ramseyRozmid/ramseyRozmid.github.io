696f04e1600a34c6761aa2fdf14e7c901402caa2
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2018-11-14 20:46:34 -0500

    Form Test Solution

---
+++ class-work/form-test/app.js
@@ -0,0 +1,9 @@
+$('#myForm').submit(handleSubmit)
+
+function handleSubmit(event) {
+  console.log(event)
+  event.preventDefault()
+  var input = $('#name-input').val()
+
+  $('#target').html(input)
+}
