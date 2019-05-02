f88a7256e2c2c807bc10dfa563d89cde7c8eb161
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-05-01 21:17:12 -0400

    trying to tpush fizzbuzz

---
+++ class-work/JS1May/loops.js
@@ -0,0 +1,23 @@
+let myAge = 24;
+
+if (myAge < 16){
+  console.log('You cannot do much outside of going to school')
+}
+else if (myAge >= 16 && myAge < 18){
+  console.log('You can drive')
+}
+else if (myAge >= 18 && myAge < 21){
+  console.log('You can vote')
+}
+else if (myAge >= 21 && myAge < 25){
+  console.log('You can drink alcohol')
+}
+else if (myAge >= 25 && myAge < 35){
+  console.log('You can rent a car')
+}
+else if (myAge >= 35 && myAge < 62){
+  console.log('You can run for president')
+}
+else if (myAge >= 62){
+  console.log('You collect social security benefits')
+}
