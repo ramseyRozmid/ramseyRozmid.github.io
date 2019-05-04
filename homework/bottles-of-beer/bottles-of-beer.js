be0146ad73901c047238e06b5039a0f03e4de5ad
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-05-04 16:07:44 -0400

    fixing scenario where increment is 2 to cover singular and plural cases

--- homework/bottles-of-beer/bottles-of-beer.js
+++ homework/bottles-of-beer/bottles-of-beer.js
@@ -5,7 +5,10 @@
 let i;
 
 for (i = 99; i >0; i--) {
-  if (i === 1) {
+  if (i === 2){
+    console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i-1} bottle of beer on the wall.`);
+}
+  else if (i === 1) {
     console.log(`${i} bottle of beer on the wall, ${i} bottle of beer. Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
 }
   else {
