f3e31bd10f45ba731a6f7cdf2c10c80db88142df
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-05-04 17:42:22 -0400

    Stretch Goals, but still getting console error for calling jQuery

--- homework/madlibs/js/madlib-console.js
+++ homework/madlibs/js/madlib-console.js
@@ -5,9 +5,22 @@ let random1 = Math.floor((Math.random() * startupX.length));
 let random2 = Math.floor((Math.random() * startupY.length));
 
 let startupGenerator = ('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
-
-//$(startupGenerator).replaceAll(#xForY);
+let favoriteStartup = [];
 
 $('#create').click(function() {
-  $('#xForY').replaceWith(startupGenerator);
+$('#xForY').replaceWith(startupGenerator)
+});
+// $('#xForY').css({
+//   'color':'#000',
+//   'font-size':'12px',
+//   'line-height':'1.4',
+//   'font-family':'Helvetica, Arial, sans-serif'});
+
+$('#save').click(function() {
+  favoriteStartup.push(startupGenerator)
+  console.log(favoriteStartup);
+});
+
+$('#print').click(function() {
+  $('#favorites').replaceWith(favoriteStartup)
 });
