5d4b8b4d81638cbb336ac5ee96f4b85f13351221
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-05-15 20:19:41 -0400

    Example objects

---
+++ class-work/JSMay13/seamless.js
@@ -0,0 +1,19 @@
+let seamlessObj = {
+                  name: 'Damas Falafel House',
+                  category: 'Grill, Lunch Specials, Mediterranian',
+                  deliveryTime: '55-65 mins',
+                  deliveryMin: '$10 min',
+                  }
+
+// console.log(seamlessObj);
+
+let weather = {
+              temp: '47',
+              sky: 'cloudy',
+              feelsLike: '41',
+              uv: '0 out of 10',
+}
+
+console.log(weather);
+
+console.log()
