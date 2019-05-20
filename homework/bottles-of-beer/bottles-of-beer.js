7b73fbffa32f5da90d39397eae5f1c1a96d56083
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-05-20 18:51:32 -0400

    99 bottles of beer HW

--- homework/bottles-of-beer/bottles-of-beer.js
+++ homework/bottles-of-beer/bottles-of-beer.js
@@ -6,12 +6,25 @@ let i;
 
 for (i = 99; i >0; i--) {
   if (i === 2){
-    console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i-1} bottle of beer on the wall.`);
+    document.getElementById("changeContent").innerHTML += `${i} bottles of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i-1} bottle of beer on the wall.` + '<br><br>'
 }
   else if (i === 1) {
-    console.log(`${i} bottle of beer on the wall, ${i} bottle of beer. Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
+    document.getElementById('changeContent').innerHTML += `${i} bottle of beer on the wall, ${i} bottle of beer. Take one down and pass it around, ${i-1} bottles of beer on the wall.` + '<br><br>'
 }
   else {
-    console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
+    document.getElementById('changeContent').innerHTML += `${i} bottles of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i-1} bottles of beer on the wall.` + '<br><br>'
 }
 };
+
+// for (num = 100; num > 0; num--) {
+//     if  (num > 2){
+//         document.getElementById("list").innerHTML +=  num + ' bottles of beer on the wall, ' + num + ' bottles of beer. Take one down and pass it around, ' + (num-1) + ' bottles of beer on the wall' + '<br>'
+//
+//      }
+//      if  (num === 2)  {
+//      document.getElementById("list").innerHTML += num + ' bottles of beer on the wall, ' + num + ' bottles of beer. Take one down and pass it around, ' + (num-1) + ' bottle of beer on the wall.'
+//      }
+//      if  (num === 1)  {
+//      document.getElementById("list").innerHTML += num + ' bottle of beer on the wall, ' + num + ' bottle of beer. Take one down and pass it around, ' + (num-1) + ' bottles of beer on the wall.'
+//      }
+// }
