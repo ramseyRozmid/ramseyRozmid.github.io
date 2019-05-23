00cf405790787474d5f24757347c39835b857130
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-05-22 21:26:09 -0400

    Start of the Weather API HW

---
+++ homework/weather-api/weatherAPI.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+    console.log( "ready!" )
+
+let userZip = $('.cityInput').val()
+
+let baseURL = ('https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?zip=')
+let token = ('&appid=3d86ff3747fab8d7d200519e32b435fe')
+
+let dataResponse
+
+$.ajax({
+    method:'GET',
+    url: `${baseURL} + ${userZip} + ${token}`,
+    dataType: 'json'
+  })
+  .done(function(dataResponse){
+
+  //log out the data
+  console.log(dataResponse);
+
+  });
+
+  $('.temp').html(dataResponse.main.temp)
+
+  $('.cityForm').submit(function(p){
+    p.preventDefault()
+
+    let weatherURL =
+
+    $.post(weatherURL,
+      {fieldInput: fieldInput}
+    ).done
+    console.log(data)
+  })
+
+})
