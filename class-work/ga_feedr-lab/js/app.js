475f2609a01ff90c019a83c40e622d8830b47c11
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-06-03 19:40:04 -0400

    Loading pacman via CDN

--- class-work/ga_feedr-lab/js/app.js
+++ class-work/ga_feedr-lab/js/app.js
@@ -1,3 +1,49 @@
-/*
-  Please add all Javascript code to this file.
-*/
+$.LoadingOverlay("show", {
+image       : "images/ajax_loader.gif",
+});
+
+setTimeout(function(){
+$.LoadingOverlay("hide");
+}, 1800);
+
+// $(document).ready(function() {
+//     console.log( "ready!" )
+//
+//     $body = $("body");
+//
+//
+//
+//
+//     // $(document).on({
+//     //     ajaxStart: function() { $body.attr('id','popUp.loader');    },
+//     //      ajaxStop: function() { $body.attr('id','popUp.loader'); }
+//     // });
+//
+//
+//
+//
+// let redditURL = ('https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/top.json')
+//
+// let dataResponse
+//
+// //$('.tempBtn').click(function(){
+//
+//   $.ajax({
+//     method:'GET',
+//     url: `${redditURL}`,
+//     dataType: 'json'
+//   })
+//
+//   .done(function(dataResponse){
+//
+//   //log out the data
+//   console.log(dataResponse);
+//
+//
+//   //$('.temp').html(fTemp)
+//
+//   $('.zipForm').submit(function(p){
+//     p.preventDefault()
+//   })
+// });
+// });
