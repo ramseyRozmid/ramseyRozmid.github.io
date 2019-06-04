08d99fe0602aa0e8273da3dd3b719eac6e626922
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-06-03 21:08:37 -0400

    In-Progress Work

--- class-work/ga_feedr-lab/js/app.js
+++ class-work/ga_feedr-lab/js/app.js
@@ -22,28 +22,49 @@ $.LoadingOverlay("hide");
 //
 //
 //
-// let redditURL = ('https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/top.json')
-//
-// let dataResponse
-//
-// //$('.tempBtn').click(function(){
-//
-//   $.ajax({
-//     method:'GET',
-//     url: `${redditURL}`,
-//     dataType: 'json'
-//   })
-//
-//   .done(function(dataResponse){
-//
-//   //log out the data
-//   console.log(dataResponse);
-//
-//
-//   //$('.temp').html(fTemp)
-//
-//   $('.zipForm').submit(function(p){
-//     p.preventDefault()
-//   })
+let redditURL = ('https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/top.json')
+
+let dataResponse
+
+  $.ajax({
+    method:'GET',
+    url: `${redditURL}`,
+    dataType: 'json',
+    statusCode: {
+    404: function() {
+      alert( "API Request Failed :(" );
+    }
+  }
+  })
+
+  .done(function(dataResponse){
+    alert('Successful API request!')
+
+  //log out the data
+  console.log(dataResponse);
+
+  //$('.temp').html(fTemp)
+
+  let workingData1 = dataResponse.data.children[0]
+  let workingData2 = dataResponse.data.children[1]
+  let workingData3 = dataResponse.data.children[2]
+  let workingData4 = dataResponse.data.children[3]
+
+  $('#articleTitle1').html(workingData1.data.title).click(function(){
+    $('#articleTitle1').attr('id','#popUp')
+    $('.container').html(workingData1.data.title)
+  })
+
+  $('#articleTitle2').html(workingData2.data.title)
+  $('#articleTitle3').html(workingData3.data.title)
+  $('#articleTitle4').html(workingData4.data.title)
+
+//When the user selects an article's title show the #popUp overlay
+//The content of the article must be inserted in the .container class inside #popUp
+//Make sure you remove the .loader class when toggling the article information in the pop-up.
+
+  // $('.zipForm').submit(function(p){
+  //   p.preventDefault()
+  })
 // });
 // });
