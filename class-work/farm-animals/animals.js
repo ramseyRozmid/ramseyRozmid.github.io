75ee59f8ab54b4f4e48700461b650e8e29a3ad97
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-06-05 21:03:16 -0400

    Working sound code push

--- class-work/farm-animals/animals.js
+++ class-work/farm-animals/animals.js
@@ -1,22 +1,39 @@
 //Starter code
 
-function Animal(sound) {
+function Animal(sound, type) {
     this.sound = sound
+    this.type = type
   }
 
-  function FarmAnimal(sound) {
-    Animal.call(this, sound)
+  function FarmAnimal(sound, type) {
+    Animal.call(this, sound, type)
   }
 
   FarmAnimal.prototype = Object.create(Animal.prototype)
 
-  var cow = new FarmAnimal('moo')
-  var sheep = new FarmAnimal('baa')
-  var dog = new FarmAnimal('bark')
-  var pig = new FarmAnimal('oink')
+  var cow = new FarmAnimal('moo', 'cow')
+  var sheep = new FarmAnimal('baa', 'sheep')
+  var dog = new FarmAnimal('bark', 'dog')
+  var pig = new FarmAnimal('oink', 'pig')
 
+  //find or slice
+  
+  $('#cow-btn').click(function() {
+  $('#animal-type').html(cow.type);
+  $('#animal-sound').html(cow.sound)
+  });
 
-  console.log(cow.sound)
-  console.log(sheep.sound)
-  console.log(dog.sound)
-  console.log(pig.sound)
+  $('#sheep-btn').click(function() {
+  $('#animal-type').html(sheep.type);
+  $('#animal-sound').html(sheep.sound)
+  });
+
+  $('#dog-btn').click(function() {
+  $('#animal-type').html(dog.type);
+  $('#animal-sound').html(dog.sound)
+  });
+
+  $('#pig-btn').click(function() {
+  $('#animal-type').html(pig.type);
+  $('#animal-sound').html(pig.sound)
+  });
