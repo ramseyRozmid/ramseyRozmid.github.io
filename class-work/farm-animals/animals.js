1dac97e7dbbefa75d52e1d19e1318e6ba198c2cb
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-06-05 20:36:23 -0400

    Adding animals within main object

--- class-work/farm-animals/animals.js
+++ class-work/farm-animals/animals.js
@@ -1,1 +1,22 @@
 //Starter code
+
+function Animal(sound) {
+    this.sound = sound
+  }
+
+  function FarmAnimal(sound) {
+    Animal.call(this, sound)
+  }
+
+  FarmAnimal.prototype = Object.create(Animal.prototype)
+
+  var cow = new FarmAnimal('moo')
+  var sheep = new FarmAnimal('baa')
+  var dog = new FarmAnimal('bark')
+  var pig = new FarmAnimal('oink')
+
+
+  console.log(cow.sound)
+  console.log(sheep.sound)
+  console.log(dog.sound)
+  console.log(pig.sound)
