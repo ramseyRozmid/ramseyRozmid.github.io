dd2173b60c51863b2009540cc7d9fc8bc37b6183
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-06-17 21:19:08 -0400

    React in-class work

---
+++ class-work/react-app-one/my-app/src/components/MyFirstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyFirstComponent extends React.Component {
+  render() {
+    return (
+    <div className="App">
+      <h2>My First Component</h2>
+    </div>
+  )
+  }
+}
+
+export default MyFirstComponent;
