f5c7e4ec1ba33300af79461e8d661558a7bff7f3
Ramsey Abdulhamid <ramsey.abdulhamid@gmail.com>
2019-06-23 11:23:21 -0400

    Working Axios JS file (casing changes)

--- class-work/react-app-one/my-app/src/components/Axios.js
+++ class-work/react-app-one/my-app/src/components/Axios.js
@@ -1,27 +1,28 @@
-// import React from 'react';
-//
-// class Axios extends React.Component {
-//
-//   state = {
-//     posts: []
-//   }
-//
-//   componentWillMount(){
-//     axios.get('https://swapi.co/api/people/')
-//       .then(res => {
-//         console.log(res)
-//         this.setState({
-//           posts:res.data.results
-//         })
-//       })
-//   }
-//
-//   render() {
-//     const { posts } = this.state
-//     return posts.map((item) => (
-//       <p> {item.name} </p>
-//     ))
-//   }
-// }
-//
-// export default Axios;
+import React from 'react';
+import axios from 'axios';
+
+class Axios extends React.Component {
+
+  state = {
+    posts: []
+  }
+
+  componentWillMount(){
+    axios.get('https://swapi.co/api/people/')
+      .then(res => {
+        console.log(res)
+        this.setState({
+          posts:res.data.results
+        })
+      })
+  }
+
+  render() {
+    const { posts } = this.state
+    return posts.map((item) => (
+      <p> {item.name} </p>
+    ))
+  }
+}
+
+export default Axios;
